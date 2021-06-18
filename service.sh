5c66e895d369874554c25b526e311c865e0503eb
Heber.crv <heber.crv@outlook.com>
2021-06-18 01:31:49 -0500

    Corrected syntax

--- service.sh
+++ service.sh
@@ -2,6 +2,8 @@
 
 #PUBLIC_IP=$(az network public-ip show -g heber-cervantes -n loadBalancer --query "ipAddress" -o tsv)
 PUBLIC_IP=$(az network public-ip show -g $CLUSTER_RESOURCE_GROUP -n loadBalancer --query "ipAddress" -o tsv)
+
+cd ..
 echo "$(cat service.yml | head -17)" > service.yml
 echo "  loadBalancerIP: $PUBLIC_IP" >> service.yml
 echo "  selector:
